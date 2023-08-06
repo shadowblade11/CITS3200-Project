98ee8ecd0090cf7d9cc842684656bcee2fe5d013
streamC31 <23011392@student.uwa.edu.au>
2023-08-06 22:26:34 +0800

    merge

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 DB_NAME = "database.db"    
 
---
+++ config.py
@@ -0,0 +1,5 @@
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SQLALCHEMY_DATA
