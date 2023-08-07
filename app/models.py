f50cdc7143efdde884c2007b58e7180ced5d96e1
xiaoxistream <23011392@student.uwa.edu.au>
2023-08-07 11:51:24 +0800

    set up database for student

--- app/models.py
+++ app/models.py
@@ -1,11 +1,24 @@
-from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+from app import db,login
+from werkzeug.security import check_password_hash, generate_password_hash
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     passwd = db.Column(db.String(128))
-    # v_code = db.Column(db.String(6))
+
+    def set_passwd(self,passwd):
+        self.password_hash = generate_password_hash(passwd)
+
+    def check_passwd(self, passwd):
+        return check_password_hash(self.password_hash, passwd)
 
     def __repr__(self):
         return f'<User {self.username}>'
--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+from flask_login import current_user, login_user, logout_user
+
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
+from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +15,14 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/login')
+@app.route('/login', methods= ["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # form = LoginForm()
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
