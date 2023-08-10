a1966cd826696b5aa81e3d03e1ed0928be6260d3
shareeennn <shareeennn@gmail.com>
2023-08-10 16:40:31 +0800

    added some debugging features

--- sqlite_blob_data/app.py
+++ sqlite_blob_data/app.py
@@ -1,67 +1,120 @@
 import os
+import platform
 import sqlite3
 from sqlite3 import Error
 
-def insertDB(filePath, fileBlob): 
+#deleteRecord and checkTables just for debugging purposes
+def deleteRecord():
+    try:
+        connect = sqlite3.connect('app.db')
+        cursor = connect.cursor()
+        print("Connected to SQLite")
+
+        # Deleting single record now
+        sql_delete_query = """DELETE from uploads"""
+        cursor.execute(sql_delete_query)
+        connect.commit()
+        print("Record deleted successfully ")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to delete record from sqlite table", error)
+    finally:
+        if connect:
+            connect.close()
+            print("the sqlite connection is closed")
+
+def checkTables():
   try:
-    conn = sqlite3.connect('app.db')
+    connect = sqlite3.connect('app.db')
     print("[INFO] : Successful connection!")
-    cur = conn.cursor()
-    sql_insert_file_query = '''INSERT INTO uploads(file_name, file_blob)
+
+    # Getting all tables from sqlite_master
+    sql_query = """SELECT name FROM sqlite_master
+    WHERE type='table';"""
+    cursor = connect.cursor()
+    cursor.execute(sql_query)
+    print("\nList of tables")
+    print(cursor.fetchall())
+    
+    # Display files in DB
+    print('\nFiles in uploads table:')
+    data=cursor.execute('''SELECT file_name FROM uploads''')
+    for column in data:
+        print(column)
+    
+  except sqlite3.Error as error:
+    print("[INFO]: Failed to execute the above query", error)    
+  finally:
+    if connect:
+        connect.close()
+
+def insertDB(filePath, fileBlob):
+  try:
+    connect = sqlite3.connect('app.db')
+    print("[INFO] : Successful connection!")
+    cur = connect.cursor()
+    sqlInsertFileQuery = '''INSERT INTO uploads(file_name, file_blob)
       VALUES(?, ?)'''
-    cur = conn.cursor()
-    cur.execute(sql_insert_file_query, (filePath, fileBlob, ))
-    conn.commit()
-    print("[INFO] : The blob for ", filePath, " is in the database.") 
+    cur = connect.cursor()
+    cur.execute(sqlInsertFileQuery, (filePath, fileBlob,))
+    connect.commit()
+    print("[INFO] : The blob for ", filePath, " is in the database.")
     lastEntry = cur.lastrowid
     return lastEntry
   except Error as e:
     print(e)
   finally:
-    if conn:
-      conn.close()
+    if connect:
+      connect.close()
     else:
-      error = "An error has occurred."
+      error = "An error has occurred"
 
 def convertBinary(filePath):
   with open(filePath, 'rb') as file:
     binary = file.read()
   return binary
 
-def readData(entry_id):
+def readBlobData(entryID):
   try:
-    conn = sqlite3.connect('app.db')
-    cur = conn.cursor()
-    print("[INFO] : Connected to SQLite to read_blob_data")
-    sql_fetch_blob_query = """SELECT * from uploads where id = ?"""
-    cur.execute(sql_fetch_blob_query, (entry_id,))
+    connect = sqlite3.connect('app.db')
+    cur = connect.cursor()
+    print("[INFO] : Connected to SQLite to readBlobData")
+    sqlFetchBlobQuery = """ SELECT * from uploads where id = ?"""
+    cur.execute(sqlFetchBlobQuery, (entryID,))
     record = cur.fetchall()
     for row in record:
-      converted_file_name = row[1]
-      photo_binarycode  = row[2]
-      # parse out the file name from converted_file_name
-      # Windows developers should reverse "/" to "\" to match your file path names 
-      last_slash_index = converted_file_name.rfind("/") + 1 
-      final_file_name = converted_file_name[last_slash_index:] 
-      writeToFile(photo_binarycode, final_file_name)
-      print("[DATA] : Image successfully stored on disk. Check the project directory. \n")
+      convertedFileName = row[1]
+      photoBinCode = row[2]
+      if platform.system() == "Windows":
+        lastSlashIndex = convertedFileName.rfind("\\") + 1
+      else:
+        lastSlashIndex = convertedFileName.rfind("/") + 1
+      finalFileName = convertedFileName[lastSlashIndex:]
+      writeToFile(photoBinCode, finalFileName)
+      print("[DATA] : Image successfully stored on disk. Check the working directory. \n")
     cur.close()
   except sqlite3.Error as error:
     print("[INFO] : Failed to read blob data from sqlite table", error)
   finally:
-    if conn:
-        conn.close()
+    if connect:
+      connect.close()
 
-def writeToFile(binary_data, file_name):
-  with open(file_name, 'wb') as file:
-    file.write(binary_data)
-  print("[DATA] : The following file has been written to the project directory: ", file_name)
+def writeToFile(binData, fileName):
+  with open(fileName, 'wb') as file:
+    file.write(binData)
+  print("[DATA] : The following file has been written to the working directory: ", fileName)
 
 def main():
-    filePath = "/Users/Shareen/recording.wav"
-    fileBlob = convertBinary(filePath)
-    lastEntry = insertDB(filePath, fileBlob)
-    readData(lastEntry)
+  filePath = "recording.wav"
+  file_blob = convertBinary(filePath)
+  lastEntry = insertDB(filePath, file_blob)
+  readBlobData(lastEntry)
+  #checkTables()
+  #deleteRecord()
 
 if __name__ == "__main__":
     main()
+
+
+
