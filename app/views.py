9041c0767c878b189007643ac3d1b3d6dc4b4d39
xiaoxistream <23011392@student.uwa.edu.au>
2023-08-17 16:09:27 +0800

    update requirements.txt

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from flask_login import current_user, login_user, logout_user
-from wtforms import ValidationError
 
 from app import app, db, verification
 from flask import render_template, redirect, url_for, flash, request, session
@@ -44,28 +43,16 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form1 = RegistrationForm()
-    form2 = VerificationForm()
-    if request.method == 'POST':
-        if form2.validate_on_submit() and form1.id.data == 8:
-            v_code = verification.generate_v_code(6)  # Generate verification code for user
-            print(v_code)
-            session['verification_code'] = v_code
-            # verification.send_v_code(email_addr=form1.id.data + '@student.uwa.edu.au', v_code=v_code)
-            flash("Verification code has been sent to your email. "
-                  "Please check your inbox or junk folder and enter the code.")
-        if form1.validate_on_submit() and form2.v_code.data is not None:
-            if session['verification_code'] == form2.v_code.data:
-                # TODO: check user's verification code
-                user = User(id=form1.id.data)
-                user.set_passwd(form1.passwd1.data)
-                return redirect(url_for('login'))
-            else:
-                print("doesnt match")
-                form2.v_code.errors = list(form2.v_code.errors)
-                form2.v_code.errors.append("Incorrect verification code")  # Add an error message to the form field
-        # db.session.add(user)
-        # db.session.commit()  # Get the data user submitted and write into the database
-        # flash("Registration successful!")
-        # return url_for('login')
-    return render_template('register.html', title='Register', form1=form1, form2=form2)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        session['id'] = form.id.data
+        session['passwd'] = form.passwd2.data
+        return redirect(url_for('verify'))
+    return render_template('register.html',form=form,title='Register')
+
+
+@app.route('/verify',methods=['POST','GET'])
+def verify():
+    form = VerificationForm()
+    print(session.get('id'))
+    return render_template('verify.html', title='Register',form=form)
