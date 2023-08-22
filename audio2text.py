b33449ae4f29ac74cd0e59b2776259264e86329c
shareeennn <shareeennn@gmail.com>
2023-08-23 04:46:13 +0800

    trying out different options

--- audio2text.py
+++ audio2text.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-import eng_to_ipa as ipa
+import epitran as epi
 
 source = "recording.wav"
 target = "recording1.wav"
@@ -8,12 +8,18 @@ r = sr.Recognizer()
 
 with sr.AudioFile(source) as source:
     audio_data = r.record(source)
-    text = r.recognize_google(audio_data)
+    text = r.recognize_google(audio_data, language="it-IT")
     print(text)
-    print(ipa.convert(text))
+    epitran = epi.Epitran('ita-Latn') 
+    print(epitran.transliterate(text)) 
+
 
 with sr.AudioFile(target) as source:
     audio_data = r.record(source)
-    text = r.recognize_google(audio_data)
+    text = r.recognize_google(audio_data, language="it-IT")
     print(text)
-    print(ipa.convert(text))
+    epitran = epi.Epitran('ita-Latn')
+    print(epitran.transliterate(text))
+
+##can use allosaurus 
+# python -m allosaurus.run -i <filename>
