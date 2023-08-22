400a94a21f6ee5d3a379507197641d2d50ef6f62
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-22 14:00:42 +0800

    added temp route (needs code)

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +18,9 @@ def login():
 @app.route('/test')
 def test():
     return render_template('main.html')
+
+
+@app.route("/save-audio")
+def save_audio():
+    data = request.data
+    
