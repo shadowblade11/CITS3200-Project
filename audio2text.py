2ddb9c7e9a417fce21f92b543c42a2c01fb7b33a
shareeennn <shareeennn@gmail.com>
2023-08-23 23:32:34 +0800

    trying out a way to get score

--- audio2text.py
+++ audio2text.py
@@ -1,6 +1,25 @@
 import speech_recognition as sr
 import epitran
 
+def checkPronounciation(sourcePhone, submittedPhone):
+    matchedPhone = 0
+    print(len(sourcePhone))
+    print(len(submittedPhone))
+    if len(sourcePhone) >= len(submittedPhone):
+        longString = sourcePhone
+        shortString = submittedPhone
+    else:
+        longString = submittedPhone
+        shortString = sourcePhone
+    
+    for i in range(len(longString)):
+        if (i < len(shortString)):
+            if (sourcePhone[i] == submittedPhone[i]):
+                matchedPhone += 1
+    
+    score = matchedPhone / len(longString)    
+    print(score)
+
 def getPhone(audioFile): 
     r = sr.Recognizer()
     with sr.AudioFile(audioFile) as source:
@@ -10,10 +29,15 @@ def getPhone(audioFile):
         epi = epitran.Epitran('ita-Latn')
         phone = epi.transliterate(text)
         print(phone)
+        return phone
 
 if __name__ == "__main__" :
+    
     source = "recording.wav"
-    getPhone(source)
+    sourcePhone = getPhone(source)
+    
     submittedAudio = "recording1.wav"
-    getPhone(submittedAudio)
+    submittedPhone = getPhone(submittedAudio)
+    checkPronounciation(sourcePhone, submittedPhone)
+
 
