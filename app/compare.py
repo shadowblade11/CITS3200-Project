95fc080455a4d6d405a3d8fd069ecd835f82e327
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-27 14:29:38 +0800

    basic function to produce soundwave image

--- app/compare.py
+++ app/compare.py
@@ -1,5 +1,7 @@
-from speech_recognition import Recognizer,AudioFile
-
+# from speech_recognition import Recognizer,AudioFile
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
 
 source = "../audio/pronunciation_it_buongiorno.wav"
 
@@ -10,6 +12,24 @@ source = "../audio/pronunciation_it_buongiorno.wav"
     
 #     text = rec.recognize_google(audio,language="it-IT")
 #     print(text)
+
+def generate_soundwave_image(file):
+    soundwave = wave.open(file,"r")
+    # print(soundwave.getframerate())
+    raw_audio = soundwave.readframes(-1)
+    raw_audio = np.frombuffer(raw_audio,"int16")
+    # print(raw_audio)
+    # print(len(raw_audio))
+
+    length = np.linspace(0,len(raw_audio) / soundwave.getframerate(), num=len(raw_audio))
+    print(length)
+
+    plt.figure(figsize=(20,5))
+    plt.title("Audio: {}".format(source))
+    plt.plot(length,raw_audio)
+    plt.savefig("test")
+
+generate_soundwave_image(source)
 
 
 # get_text(source)
