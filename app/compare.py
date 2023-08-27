70cf2596df4e7f6f207013f1fda2897ee73bbcba
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-27 14:18:40 +0800

    scrapped compare function

--- app/compare.py
+++ app/compare.py
@@ -3,13 +3,13 @@ from speech_recognition import Recognizer,AudioFile
 
 source = "../audio/pronunciation_it_buongiorno.wav"
 
-def get_text(audio_file):
-    rec = Recognizer()
-    with AudioFile(audio_file) as audio_track:
-        audio = rec.record(audio_track)
+# def get_text(audio_file):
+#     rec = Recognizer()
+#     with AudioFile(audio_file) as audio_track:
+#         audio = rec.record(audio_track)
     
-    text = rec.recognize_google(audio,language="it-IT")
-    print(text)
+#     text = rec.recognize_google(audio,language="it-IT")
+#     print(text)
 
 
-get_text(source)
+# get_text(source)
