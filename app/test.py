03ddc255b087ba46c704b91e7489c85b4f0057cc
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-28 22:46:22 +0800

    works on powershell but not subprocesses

--- app/test.py
+++ app/test.py
@@ -1,27 +1,3 @@
 import os
 import subprocess
-ffmpeg_path = '..\FFmpeg\bin'
-
-# # os.environ["FFmpeg"] = ffmpeg_path
-
-
-
-current_path = os.environ.get("PATH","")
-# print(ffmpeg_path)
-new_path = f"{ffmpeg_path}{os.pathsep}{current_path}"
-os.environ["PATH"] = new_path
-# flag = os.environ.get("ffmpeg")
-# print(flag)
-# if flag:
-#     version = "-version"
-    # command = [flag,version]
-    # print(command)
-# subprocess.run(f"ffmpeg")
-# else:
-#     print("flag not set")
-
-env_variables = os.environ
-
-# # Print each environment variable
-for key, value in env_variables.items():
-    print(f"{key}: {value}")
+subprocess.run(f".\ffmpeg.exe -version")
