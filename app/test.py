51d92a80b50ba8d964d7042b1d46b53457056db8
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-28 22:58:40 +0800

    working (concept) of ffmpeg being used without the need to install the path

--- app/test.py
+++ app/test.py
@@ -1,3 +1,14 @@
-import os
 import subprocess
-subprocess.run(f".\ffmpeg.exe -version")
+import ffmpeg
+from pydub import AudioSegment
+src = "../audio/sound.mp3"
+dst = "../audio/test.wav"
+i = ffmpeg.input(src)
+o = ffmpeg.output(i,dst)
+# print(o)
+ffmpeg.run(o)
+# convert wav to mp3                                                            
+# sound = AudioSegment.from_mp3(src)
+# sound.export(dst, format="wav")
+# input_file = ffmpeg.input("../audio/sound.mp3")
+# subprocess.run(f".\ffmpeg.exe -version")
