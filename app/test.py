df3a8ea200fe65611a5935e08335fb6b9922e2a0
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-28 22:59:38 +0800

    comments and removed code

--- app/test.py
+++ app/test.py
@@ -3,12 +3,7 @@ import ffmpeg
 from pydub import AudioSegment
 src = "../audio/sound.mp3"
 dst = "../audio/test.wav"
-i = ffmpeg.input(src)
-o = ffmpeg.output(i,dst)
-# print(o)
-ffmpeg.run(o)
-# convert wav to mp3                                                            
-# sound = AudioSegment.from_mp3(src)
-# sound.export(dst, format="wav")
-# input_file = ffmpeg.input("../audio/sound.mp3")
-# subprocess.run(f".\ffmpeg.exe -version")
+i = ffmpeg.input(src) #get input file path
+o = ffmpeg.output(i,dst) #get output file path
+
+ffmpeg.run(o) #run the command
