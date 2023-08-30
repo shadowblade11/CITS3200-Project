04823c9094f992d4dac5a971502bcbbae5615b46
shareeennn <shareeennn@gmail.com>
2023-08-30 14:18:16 +0800

    renamed file for easier viewing

--- audioToText.py
+++ audioToText.py
@@ -35,7 +35,7 @@ def getPhone(audioFile):
 
 if __name__ == "__main__" :
     
-    source = "source.wav" #change to the file you need
+    source = "audiofiles_wav/molto_bene_grazie.wav" #change to the file you need
     sourcePhone = IPAString(unicode_string=getPhone(source)) ## converting the unicode string to IPA string (done by the package)
     
     submittedAudio = "recording.wav" #change to the file you need
