62e4ce51364e1ef44c9c1b8018ae81a71a720a84
shadowblade11 <shadow_blade12@hotmail.com>
2023-08-30 13:42:41 +0800

    updated to accept any file extension, also tested to make sure the audio files provided works

--- app/compare.py
+++ app/compare.py
@@ -18,24 +18,30 @@ def generate_soundwave_image(file, filename):
     # plt.title("Audio: {}".format(source)) #hide the title
     plt.plot(length,raw_audio)
     plt.axis('off') #hide axis
-    filename = filename.split(".")[0]
-    # print(filename)
+    print(filename)
     plt.savefig(f"../images/{filename}", transparent = True)
 
 
 def convert_to_wav_working_format(file,filename):
     i = ffmpeg.input(file) #get input file path
-    # o = ffmpeg.output(i,f"../audio/{filename}",af="silenceremove=1:0:-50dB") #get output file path, also removes the silent noise at the start
-    o = ffmpeg.output(i,f"../audio/{filename}") #no silent noise removed
+    # o = ffmpeg.output(i,f"../audio/{filename}.wav",af="silenceremove=1:0:-50dB") #get output file path, also removes the silent noise at the start
+    o = ffmpeg.output(i,f"../audio/{filename}.wav") #no silent noise removed
     ffmpeg.run(o, overwrite_output=True, quiet=True) #run the command
     print("successful")
 
 
-list_of_audio = ["sound.wav","buon_pomeriggio.wav","buongiorno.wav"]
+list_of_audio = ["1 come ti chiami.m4a","2 come stai.m4a","3 questo e Matteo.m4a"]
 filename=list_of_audio[1]
-path = "../audio/"
-src = path+f"/imported/{filename}"
-dst = path+f"{filename}"
+filename,suffix = filename.split(".")
+
+if suffix != "wav":
+    path = "../audio/"
+    src = path+f"/imported/{filename}.{suffix}"
+    dst = path+f"{filename}.wav"
+else:
+    path = "../audio/"
+    src = path+f"/imported/{filename}.wav"
+    dst = path+f"{filename}.wav"
 
 convert_to_wav_working_format(src,filename)
 generate_soundwave_image(dst,filename)
