53cc2efc1efcae692bb73b4ecc55f7a3ccc5c38b
shareeennn <shareeennn@gmail.com>
2023-09-01 14:49:07 +0800

    trying something

--- visualWave.py
+++ visualWave.py
@@ -35,6 +35,48 @@ def phone_recognize_file(path, emit=1.2, lang='eng'):
     phones['color'] = phones['consonant'].replace({True: 'green', False: 'red'})
     return phones
 
+def load_spectrogram(path, hop_length=1024, sr=16000, n_mels=64, ref=numpy.max, **kwargs):
+    y, sr = librosa.load(path, sr=sr)
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, **kwargs)
+    S_db = librosa.power_to_db(S, ref=ref)
+    tt = numpy.arange(0, S_db.shape[1]) * (hop_length / sr)
+    print(tt.shape)
+    df = pandas.DataFrame(S_db.T, index=pandas.Series(tt, name='time'))
+    return df
+
+def plot_events(ax, df, start='start', end='end', color=None, annotate=None,
+                label=None, alpha=0.2, zorder=-1,
+                text_kwargs={}, **kwargs):
+
+    import itertools
+    import seaborn
+    palette = itertools.cycle(seaborn.color_palette())
+    
+    def valid_time(dt):
+        return not pandas.isnull(dt)
+
+    for idx, row in df.iterrows():
+        s = row[start]
+        e = row[end]
+        
+        if color is None:
+            c = next(palette)
+        else:
+            c = row[color]
+        
+        label = None
+        if valid_time(s) and valid_time(e):
+            ax.axvspan(s, e, label=label, color=c, alpha=alpha, zorder=zorder)
+        if valid_time(e):
+            ax.axvline(e, label=label, color=c, alpha=alpha, zorder=zorder)
+        if valid_time(s):
+            ax.axvline(s, label=label, color=c, alpha=alpha, zorder=zorder)
+
+        import matplotlib.transforms
+        trans = matplotlib.transforms.blended_transform_factory(ax.transData, ax.transAxes)
+        if annotate is not None:
+            ax.text(s, 1.05, row[annotate], transform=trans, **text_kwargs)
+
 if __name__ == '__main__':
     #submittedPath = 'recording.wav'
     #submittedPhones = phone_recognize_file(submittedPath)
@@ -45,3 +87,11 @@ if __name__ == '__main__':
     sourcePhones = phone_recognize_file(sourcePath)
     print(sourcePhones)
 
+    hop_length = 512
+    spectrogram = load_spectrogram(sourcePath, hop_length=hop_length, fmax=4000)
+
+    fig, (spec_ax, label_ax) = plt.subplots(2, figsize=(20, 8), sharex=True)
+    librosa.display.specshow(spectrogram.T.values, ax=spec_ax, sr=16000, fmax=4000, hop_length=hop_length, x_axis='time')
+    plot_events(label_ax, sourcePhones, color='color', annotate='label')
+
+    fig.savefig('output_source.png', bbox_inches='tight')
