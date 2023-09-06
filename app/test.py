00cf3ebef49d5ba0e246539c22fd83d07a4d98f7
shadowblade11 <shadow_blade12@hotmail.com>
2023-09-06 12:09:12 +0800

    removed test file

--- app/test.py
+++
@@ -1,7 +0,0 @@
-import ffmpeg
-src = "../audio/sound.mp3"
-dst = "../audio/test.wav"
-i = ffmpeg.input(src) #get input file path
-o = ffmpeg.output(i,dst) #get output file path
-
-ffmpeg.run(o) #run the command
