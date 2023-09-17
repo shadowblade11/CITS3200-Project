1ccb600d627a5735acac6dc6ceb9115e227989fb
shadowblade11 <shadow_blade12@hotmail.com>
2023-09-17 14:58:47 +0800

    issue with getting test python file to call functions located in another folder

---
+++ test/testInstallation.py
@@ -0,0 +1,15 @@
+# from app.conversion import convert_to_wav_working_format
+# from app.produceImage import generate_soundwave_image
+
+import sys
+import ffmpeg
+
+
+def test():
+    print(sys.path)
+
+
+
+
+if __name__ == "__main__":
+    test()
