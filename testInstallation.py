5e204177135e2e5e31875d0664a3534b41da67c9
shadowblade11 <shadow_blade12@hotmail.com>
2023-09-17 15:03:58 +0800

    workaround, having the python file outside the test folder (not an ideal solution)

---
+++ testInstallation.py
@@ -0,0 +1,14 @@
+from app.conversion import convert_to_wav_working_format
+from app.produceImage import generate_soundwave_image
+
+import ffmpeg
+
+
+def test():
+    print("hello")
+
+
+
+
+if __name__ == "__main__":
+    test()
