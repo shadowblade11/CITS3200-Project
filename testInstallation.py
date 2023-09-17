1a3c8c06161ce9fe848bb9974fc7c3daa8d3f53b
shadowblade11 <shadow_blade12@hotmail.com>
2023-09-17 15:29:43 +0800

    added test for image production

--- testInstallation.py
+++ testInstallation.py
@@ -4,15 +4,22 @@ from app.produceImage import generate_soundwave_image
 import unittest
 import ffmpeg
 
+from PIL import Image
+
 class TestClass(unittest.TestCase):
     def testingConversionOfAudio(self):
         state = convert_to_wav_working_format("./test/9 non c'e' male.m4a","./test/9 non c'e' male.wav")
         self.assertEqual(state,0)
     def testingProductionOfImage(self):
-        pass
-        # generate_soundwave_image("./test/9 non c'e' male.wav","./test","9 non c'e' male.png")
+        generate_soundwave_image("./test/9 non c'e' male.wav","./test","9 non c'e' male.png")
+        expected_image_filename = "./test/ideal.png"
+        expected_image = Image.open(expected_image_filename)
 
+        generated_image_filename = "./test/9 non c'e' male.png"
+        generated_image = Image.open(generated_image_filename)
 
+        self.assertEqual(generated_image.size, expected_image.size)
+        self.assertEqual(list(generated_image.getdata()), list(expected_image.getdata()))
 
 
 if __name__ == "__main__":
