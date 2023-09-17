aaf7a4bfadc7318f6a42ebd8697f8d8e6a3bd4f7
shadowblade11 <shadow_blade12@hotmail.com>
2023-09-17 15:20:21 +0800

    adding unit tests

--- testInstallation.py
+++ testInstallation.py
@@ -1,14 +1,20 @@
 from app.conversion import convert_to_wav_working_format
 from app.produceImage import generate_soundwave_image
 
+import unittest
 import ffmpeg
 
-
-def test():
-    print("hello")
+class TestClass(unittest.TestCase):
+    def testingConversionOfAudio(self):
+        state = convert_to_wav_working_format("./test/9 non c'e' male.m4a","./test/9 non c'e' male.wav")
+        self.assertEqual(state,0)
+    def testingProductionOfImage(self):
+        pass
+        # generate_soundwave_image("./test/9 non c'e' male.wav","./test","9 non c'e' male.png")
 
 
 
 
 if __name__ == "__main__":
-    test()
+    unittest.main()
+    generate_soundwave_image("./test/9 non c'e' male.wav","./test","9 non c'e' male.png")
