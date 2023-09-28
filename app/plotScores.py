4187ac2aafa2b9cfcc7c22f827193f15729606b0
Nicholas Davies (Rickk#5520) <nicholas.john.davies42@gmail.com>
2023-09-28 11:49:48 +0800

    Sample code so far, refined a little

--- app/plotScores.py
+++ app/plotScores.py
@@ -2,25 +2,52 @@ import wave
 import matplotlib.pyplot as plt
 import numpy as np
 
-def generate_soundwave_image(file, output_path, filename):
-    plt.switch_backend('Agg')
-    soundwave = wave.open(file,"r")
-    # print(soundwave.getframerate())
-    raw_audio = soundwave.readframes(-1)
-    raw_audio = np.frombuffer(raw_audio,"int16")
-    # print(raw_audio)
-    # print(len(raw_audio))
+def plot_aggregate_scores(weeks, output_path, filename):
+    """
+    For now format is a little bit of a mystery so just making assumptions that can easily be fixed later
+    Plots average scores per week for all students as bar graph and saves as .png
+    Parameters:
+    scores -- A list of students' scores -- assume their . like [{student1's scores}, {student2's scores}...]
+        where objects are of form {"Score": [list of scores], "name":"student name"} still more to be figured out. 
+    output_path -- the path to output the image.
+    filename -- The name of the file to keep the image.
+    """
+    
+    avgs = []
+    # need to figure out what to put here
 
-    length = np.linspace(0,len(raw_audio) / soundwave.getframerate(), num=len(raw_audio))
-    # print(length)
+    plt.switch_backend('Agg')
+
+    length = np.linspace(0, avgs.length, num=avgs.length)
 
     plt.figure(figsize=(20,5))
-    # plt.title("Audio: {}".format(source)) #hide the title
-    plt.plot(length,raw_audio)
-    plt.axis('off') #hide axis
-    # print(filename)
+    plt.title(f"Average Scores")
+    plt.plot(length, numpy.array(avgs))
     
-    plt.savefig(f"{output_path}/{filename}", transparent = True)
+    plt.savefig(f"{output_path}/{filename}")
     plt.close()
+    return
+
+
+def plot_individual_scores(scores, output_path, filename):
+    """
+    Plots individual scores for students as line graph and saves as .png
+    Parameters:
+    scores -- A list of students' scores. like [{student1's scores}, {student2's scores}...]
+        where objects are of form {"Score": [list of scores], "name":"student name"} still more to be figured out. 
+    output_path -- the path to output the image.
+    filename -- The name of the file to keep the image.
+    """
+
+    # plt.switch_backend('Agg')
+
+    # length = np.linspace(0, .length, num=avgs.length)
+
+    # plt.figure(figsize=(20,5))
+    # plt.title(f"Average Scores")
+    # plt.plot(length, numpy.array(avgs))
+    
+    # plt.savefig(f"{output_path}/{filename}")
+    # plt.close()
 
     return 0
