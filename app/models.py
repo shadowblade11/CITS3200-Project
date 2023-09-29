e2b86d01f22fac3f5542151c05dbf9ec32cdd79f
shadowblade11 <shadow_blade12@hotmail.com>
2023-09-29 12:41:03 +0800

    commented out all tables beside user

--- app/models.py
+++ app/models.py
@@ -66,74 +66,74 @@ def load_user(id):
     return User.query.get(id)
 
 
-class Complete(DB_Queries):
-    completed_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
-    test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
-    # user = db.relationship('User', backref='complete')
-    completed = db.Column(db.Boolean)
+# class Complete(DB_Queries):
+#     completed_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
+#     test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
+#     # user = db.relationship('User', backref='complete')
+#     completed = db.Column(db.Boolean)
 
-    # def __init__(self, user):
-    #     self.user_id = user
-    #     self.completed = False
-    def __repr__(self):
-        return f'<id {self.completed_id}, user_id {self.user_id}, test_id {self.test_id}, completed {self.completed}>'
-
-
-class Feedback(DB_Queries):
-    feedback_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    feedback = db.Column(db.String(512))
-    test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
-    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
-    # user = db.relationship('User', backref='feedback')
-    def __repr__(self):
-        return f'<id {self.feedback_id}, feedback {self.feedback}, test_id {self.test_id}, user_id {self.user_id}>'
-    # def __init__(self, user):
-    #     self.feedback = None
-    #     self.user_id = user
+#     # def __init__(self, user):
+#     #     self.user_id = user
+#     #     self.completed = False
+#     def __repr__(self):
+#         return f'<id {self.completed_id}, user_id {self.user_id}, test_id {self.test_id}, completed {self.completed}>'
 
 
-class Score(DB_Queries):
-    score_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
-    user_score = db.Column(db.Integer)
-    sys_score = db.Column(db.Integer)
-    attempt_chosen = db.Column(db.Integer)
-
-    def __repr__(self):
-        return f'<score_id {self.score_id}, user_id {self.user_id}, question_id {self.question_id}, user_score {self.user_score}, etc>'
-    # def __init__(self, user):
-    #     self.sys_score, self.user_score, self.attempt_chosen = 0, 0, 0
-    #     self.user_id = user
+# class Feedback(DB_Queries):
+#     feedback_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     feedback = db.Column(db.String(512))
+#     test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
+#     user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
+#     # user = db.relationship('User', backref='feedback')
+#     def __repr__(self):
+#         return f'<id {self.feedback_id}, feedback {self.feedback}, test_id {self.test_id}, user_id {self.user_id}>'
+#     # def __init__(self, user):
+#     #     self.feedback = None
+#     #     self.user_id = user
 
 
-class Test(DB_Queries):
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    week_number = db.Column(db.Integer)
-    test_name = db.Column(db.String(128))
-    due_date = db.Column(db.String(128))  # dd/mm/yy
-    number_of_questions = db.Column(db.Integer)
-    questions = db.relationship('Question', backref='test')
+# class Score(DB_Queries):
+#     score_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable=False)
+#     question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+#     user_score = db.Column(db.Integer)
+#     sys_score = db.Column(db.Integer)
+#     attempt_chosen = db.Column(db.Integer)
 
-    def __repr__(self):
-        return f'<id {self.id}, wk {self.week_number}, testname {self.test_name}, questions {self.questions}>'
-    # def __init__(self, week_no, test_name, due_date, no_of_qs):
-    #     self.test_name = test_name
-    #     self.due_date = due_date
-    #     self.number_of_questions = no_of_qs
-    #     self.week_number = week_no
+#     def __repr__(self):
+#         return f'<score_id {self.score_id}, user_id {self.user_id}, question_id {self.question_id}, user_score {self.user_score}, etc>'
+#     # def __init__(self, user):
+#     #     self.sys_score, self.user_score, self.attempt_chosen = 0, 0, 0
+#     #     self.user_id = user
 
 
-class Question(DB_Queries):
-    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-    question_name = db.Column(db.String(128))
-    difficulty = db.Column(db.Integer)
-    test_id = db.Column(db.Integer, db.ForeignKey('test.id'))
+# class Test(DB_Queries):
+#     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     week_number = db.Column(db.Integer)
+#     test_name = db.Column(db.String(128))
+#     due_date = db.Column(db.String(128))  # dd/mm/yy
+#     number_of_questions = db.Column(db.Integer)
+#     questions = db.relationship('Question', backref='test')
 
-    def __repr__(self):
-        return f'<id {self.id}, test_id {self.test_id}>'
-    # def __init__(self, question_name, test_id, difficulty):
-    #     self.question_name = question_name
-    #     self.test_id = test_id
-    #     self.difficulty = difficulty
+#     def __repr__(self):
+#         return f'<id {self.id}, wk {self.week_number}, testname {self.test_name}, questions {self.questions}>'
+#     # def __init__(self, week_no, test_name, due_date, no_of_qs):
+#     #     self.test_name = test_name
+#     #     self.due_date = due_date
+#     #     self.number_of_questions = no_of_qs
+#     #     self.week_number = week_no
+
+
+# class Question(DB_Queries):
+#     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+#     question_name = db.Column(db.String(128))
+#     difficulty = db.Column(db.Integer)
+#     test_id = db.Column(db.Integer, db.ForeignKey('test.id'))
+
+#     def __repr__(self):
+#         return f'<id {self.id}, test_id {self.test_id}>'
+#     # def __init__(self, question_name, test_id, difficulty):
+#     #     self.question_name = question_name
+#     #     self.test_id = test_id
+#     #     self.difficulty = difficulty
