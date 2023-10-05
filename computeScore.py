ae0880377968caa13d49b5d40381afd6479b80b4
shareeennn <shareeennn@gmail.com>
2023-10-05 09:23:01 +0800

    updated again for testing

--- computeScore.py
+++ computeScore.py
@@ -3,14 +3,14 @@ from analyseDF import getScores
 
 
 if __name__ == "__main__":
-    sourceFile = 'audio/1 come ti chiami.m4a'
-    submittedFile = 'audio/3 questo e Matteo.m4a'
+    sourceFile = 'audio/1_come_ti_chiami.wav'
+    submittedFile = 'audio/2_come_stai.wav'
 
     print("\nAssessing now.... Please wait...\n")
     audio_to_text_score = convertAudio(sourceFile, submittedFile)
     ratio_score, duration_score, phone_score = getScores(sourceFile, submittedFile)
     #just to see the individual scores returned
-    print(audio_to_text_score,ratio_score, duration_score, phone_score)
+    #print(audio_to_text_score,ratio_score, duration_score, phone_score)
 
     OverallScore = round((audio_to_text_score * 0.45) + (ratio_score * 0.18) + (duration_score * 0.18) + (phone_score * 0.19))
     print("Your score is:", OverallScore, "\n")
